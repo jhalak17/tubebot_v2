fdba377fc2164bdd1bd394ae80b72e7abfde93d9
rahul-rana96 <rahul.chauhan@jll.com>
2025-03-21 16:54:19 +0530

    first commit

--- demo.py
+++
@@ -1,7 +0,0 @@
-from pytrends.request import TrendReq
-
-pytrends = TrendReq()
-realtime_india = pytrends.realtime_trending_searches(pn='IN')
-print(realtime_india)
-realtime_india_top10 = realtime_india.head(10)
-print(realtime_india_top10)
